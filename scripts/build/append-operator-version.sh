4563d394453ecc2519d5d8ad0a01853b6e94e0e0
Naveen Bharadwaj R <naveen.ramesh@netcracker.com>
2025-04-29 12:46:53 +0530

    fix: linter issues

--- scripts/build/append-operator-version.sh
+++ scripts/build/append-operator-version.sh
@@ -3,20 +3,20 @@
 if [[ "$OSTYPE" == "darwin"* ]]; then
   # Add the custom annotation
   find charts/qubership-prometheus-adapter-operator/crds -name '*.yaml' -exec sed -i '' -e "/^    controller-gen.kubebuilder.io.version.*/a\\
-    qubership-prometheus-adapter-operator.monitoring.qubership.org/version: $VERSION" {} +
+        qubership-prometheus-adapter-operator.monitoring.qubership.org/version: $VERSION" {} +
 
   # Add the labels right after 'annotations:'
   find charts/qubership-prometheus-adapter-operator/crds -name '*.yaml' -exec sed -i '' -e "/^  annotations:/a\\
-  labels:\\
-    app.kubernetes.io/component: qubership-prometheus-adapter-operator\\
-    app.kubernetes.io/part-of: monitoring" {} +
+    labels:\\
+        app.kubernetes.io/component: qubership-prometheus-adapter-operator\\
+        app.kubernetes.io/part-of: monitoring" {} +
 else
   # Linux
   find charts/qubership-prometheus-adapter-operator/crds -name '*.yaml' -exec sed -i "/^    controller-gen.kubebuilder.io.version.*/a\\
-    qubership-prometheus-adapter-operator.monitoring.qubership.org/version: $VERSION" {} +
+        qubership-prometheus-adapter-operator.monitoring.qubership.org/version: $VERSION" {} +
 
   find charts/qubership-prometheus-adapter-operator/crds -name '*.yaml' -exec sed -i "/^  annotations:/a\\
-  labels:\\
-    app.kubernetes.io/component: qubership-prometheus-adapter-operator\\
-    app.kubernetes.io/part-of: monitoring" {} +
+    labels:\\
+        app.kubernetes.io/component: qubership-prometheus-adapter-operator\\
+        app.kubernetes.io/part-of: monitoring" {} +
 fi
